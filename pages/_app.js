bfd98a88724b60dc6ae12c92c48e3a9a30aa9e9f
carlo <lobotomisador@gmail.com>
2021-12-09 20:00:47 -0600

    adds more required files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp ({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <script type='text/javascript' src='tawk.js' />
+        <link rel='icon' href='totaldevs-logo.png' />
+        <meta name='totalves' content='&nbsp;' />
+        <title>totaldevs</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
