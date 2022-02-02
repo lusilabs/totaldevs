d1af733c9376d904694f6602dcb92720cabe3445
carlo <lobotomisador@gmail.com>
2022-02-01 23:06:11 -0600

    actions

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -4,15 +4,26 @@ import { httpsCallable } from 'firebase/functions'
 import { toast } from 'react-toastify'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
+import { useDocuments } from '@/utils/hooks'
+import { getDoc, getDocs, collection, doc, query, limit, where, orderBy } from '@firebase/firestore'
+import { SuspensePlaceholders } from '@/components/suspense'
+import Link from 'next/link'
 // import DashboardActions from '@/components/dashboardactions'
 
 const generateOnboardingURL = httpsCallable(functions, 'stripe-generateOnboardingURL')
+
+const queryConstraints = [where('seen', '==', false), orderBy('priority', 'desc'), limit(10)]
 
 function Dashboard ({ userDoc, setIsPageLoading, ...props }) {
   const router = useRouter()
   const [isStripeBannerActive, setIsStripeBannerActive] = useState(true)
   const [isInviteBannerActive, setIsInviteBannerActive] = useState(true)
   const [isProfileBannerActive, setIsProfileBannerActive] = useState(true)
+  const [actions, actionsLoaded, _ar] = useDocuments({ userDoc, docs: 'actions', queryConstraints })
+  const handleClickOnAction = action => {
+
+  }
+
   const handleDevStripeOnboarding = async userDoc => {
     setIsPageLoading(true)
     const { data: url } = await generateOnboardingURL(userDoc)
@@ -39,7 +50,38 @@ function Dashboard ({ userDoc, setIsPageLoading, ...props }) {
       {isProfileBannerActive && <Banner name='profile-complete' color='bg-red-400' text='your profile is incomplete' buttonText='go to profile' href='/profile' handleClose={() => setIsProfileBannerActive(false)} />}
       {userDoc.role === 'explorer' && isInviteBannerActive && <Banner name='explorer-invites' color='bg-indigo-600' text='you have new gift developer invites!' buttonText='invite a dev!' href='/invites' handleClose={() => setIsInviteBannerActive(false)} />}
       {/* <DashboardActions userDoc={userDoc} setIsPageLoading={setIsPageLoading} {...props} /> */}
+      {!actionsLoaded && <SuspensePlaceholders />}
+      {actionsLoaded && actions.length === 0 && <EmptyDashboardView />}
+      {actionsLoaded && actions.length > 0 && actions.map((a, aix) => <ActionView action={a} key={a.id} handleClickOnAction={handleClickOnAction} />)}
+
     </div>
+  )
+}
+
+const EmptyDashboardView = () => {
+  return (
+    <div className='flex flex-col items-center'>
+      <img src='/astronaut.png' className='w-40 mt-4' />
+      <h4 className='text-gray-500'>up to date!</h4>
+    </div>
+  )
+}
+
+const ActionView = ({ action, handleClickOnAction, ...props }) => {
+  return (
+    <div className='group relative' onClick={() => handleClickOnAction(action)}>
+        <div className='w-full min-h-20 h-20 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none'>
+          <img src={action.photoURL} alt={action.photoURL} className='w-full h-full object-center object-cover lg:w-full lg:h-full' />
+        </div>
+        <div className='mt-4 flex justify-between items-start'>
+          <p className='mt-1 text-sm text-gray-500'>{action.title}</p>
+          <p className='text-sm font-medium text-gray-900'>$ {action.salary} </p>
+          {action.status === 'good' &&
+            <span className='px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800'> up to date.  </span>}
+          {action.status === 'seeking' &&
+            <span className='px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800'> review </span>}
+        </div>
+      </div>
   )
 }
 
--- utils/hooks.js
+++ utils/hooks.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { storage, db } from '@/utils/config'
-import { collection, query, where, getDocs, orderBy, limit, doc, getDoc } from 'firebase/firestore'
+import { collection, query, where, getDocs, orderBy, limit, doc, getDoc, onSnapshot } from 'firebase/firestore'
 
-export function useDocuments ({ userDoc, docs, queryConstraints = [], ...props }) {
+export function useDocuments ({ userDoc, docs, queryConstraints = [], ...props }, dependencies = []) {
   const [documents, setDocuments] = useState([])
+  const [documentsLoaded, setDocumentsLoaded] = useState(false)
   const retrieveDocuments = async () => {
     const q = query(collection(db, docs), ...queryConstraints)
     const querySnapshot = await getDocs(q)
@@ -12,10 +13,37 @@ export function useDocuments ({ userDoc, docs, queryConstraints = [], ...props }
       snaps.push({ id: doc.id, ...doc.data() })
     })
     setDocuments(snaps)
+    setDocumentsLoaded(true)
   }
 
   useEffect(() => {
-    retrieveDocuments()
-  }, [userDoc])
-  return documents
+    if (!documentsLoaded && dependencies.every(Boolean)) retrieveDocuments()
+  }, [documentsLoaded, dependencies])
+
+  const refresh = () => {
+    setDocumentsLoaded(false)
+  }
+  return [documents, documentsLoaded, refresh]
+}
+
+export function useDocument ({ collection, docID }, dependencies = []) {
+  const [document, setDocument] = useState({})
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    if (!loaded && docID && dependencies.every(Boolean)) {
+      const ref = doc(db, collection, docID)
+      onSnapshot(ref, doc => {
+        if (doc.exists) {
+          const data = doc.data()
+          setDocument(data)
+        }
+      })
+      setLoaded(true)
+    }
+  }, [loaded, docID, dependencies])
+
+  const refresh = () => setLoaded(false)
+
+  return [document, loaded, refresh]
 }
