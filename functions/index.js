93dcc92170bd2eb4e51ef51832689216c305fade
Alejandro Sosa <asosacas@gmail.com>
2022-04-20 22:44:26 -0500

    Adding company mail in job documents during anon conversion

--- functions/index.js
+++ functions/index.js
@@ -258,15 +258,6 @@ exports.handleUserLogin = functions.https.onCall(async (data, ctx) => {
           to: ['talent@totaldevs.com'],
           createdAt: new Date().toISOString()
         })
-      // we have to add the company data to the job they just posted anonymously
-      admin
-        .firestore()
-        .collection('jobs')
-        .where('uid', '==', uid)
-        .get()
-        .then(snap => {
-          snap.docs.forEach(doc => doc.ref.update({ company: uid, companyEmail: data.email }))
-        })
     }
   } else {
     // try redeeming invites by updating the user doc, so the listener can fire.
@@ -285,35 +276,15 @@ exports.handleAnonUserConversion = functions.https.onCall(async (data, ctx) => {
     .collection('users')
     .doc(uid)
   await uref.update({ ...data })
-})
-
-exports.getAssignments = functions.https.onCall(async (data, ctx) => {
-  const db = admin.firestore()
-  const uid = ctx.auth.uid
-  const assignments = await db.collection('matches').where('dev', '==', uid)
-    .get().then((querySnapshot) => {
-      return querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }))
-    })
-  const actors = await db.collection('users')
-    .where('uid', 'in',
-      [...new Set(assignments.reduce((result, assignment) => [...result, assignment.explorer, assignment.company], []))]
-    )
-    .get().then((querySnapshot) => {
-      return querySnapshot.docs.reduce((result, doc) => ({ ...result, [doc.id]: doc.data() }), {})
-    })
-  const jobs = await db.collection('jobs')
-    .where('__name__', 'in', assignments.map((assignment) => assignment.job))
-    .get().then((querySnapshot) => {
-      return querySnapshot.docs.reduce((result, doc) => ({ ...result, [doc.id]: doc.data() }), {})
-    })
-
-  return assignments.map((assignment) => (
-    {
-      ...assignment,
-      explorer: actors[assignment.explorer],
-      company: actors[assignment.company],
-      job: jobs[assignment.job]
-    }))
+  // we have to add the company data to the job they just posted anonymously
+  admin
+  .firestore()
+  .collection('jobs')
+  .where('uid', '==', uid)
+  .get()
+  .then(snap => {
+    snap.docs.forEach(doc => doc.ref.update({ company: uid, companyEmail: data.email }))
+  })
 })
 
 const triggerOnUpdate = ({ document, fieldToSearch, valueToSearch, destinationField, latestObject }) => {
