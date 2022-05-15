b02e87b4dd0040316e534feaf0a7b1ca8888dbcd
carlo <lobotomisador@gmail.com>
2022-05-15 13:27:27 -0500

    wip payment terms

---
+++ pages/payment_terms.js
@@ -0,0 +1,74 @@
+import Footer from '@/components/footer.js'
+import Link from 'next/link'
+export default function Privacy () {
+  return (
+      <>
+          <div className='flex-shrink-0 bg-black'>
+              <Link href='/'>
+                  <img
+                      className='h-12 w-12'
+                      src='/logo-small.png'
+                      alt='logo'
+                    />
+                </Link>
+            </div>
+          <div className='text-center m-4'>
+              <h1>Payment Terms</h1>
+              <p>Last updated: May 14, 2022</p>
+              <p>This Payment Terms describes Our policies and procedures on the collection, use and disclosure of Your card information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
+              <h1>Upfront payment</h1>
+              <h2>Procedure</h2>
+              Once you accept and submit your card information
+                <p>Upfront payment del 20%?
+                  Como manejamos este payment?
+                  Creando el primer modelo como payment luego de una semana subscription una vez que acepten ambas partes de nuevo
+
+                  Handling the first week
+                  Terms and conditions Criterios de 'professional reasons'
+                  acordar ciertos criterios de dejar el trabajo y seguir en la comunidad o de terminacion de trabajo por company o por dev
+
+                  Meter ya contratos reales
+                </p>
+
+              <h1>7 day Trial period</h1>
+              <h2 />
+              Once you accept and submit your
+                <p>Upfront payment del 20%?
+                  Como manejamos este payment?
+                  Creando el primer modelo como payment luego de una semana subscription una vez que acepten ambas partes de nuevo
+
+                  Handling the first week
+                  Terms and conditions Criterios de 'professional reasons'
+                  acordar ciertos criterios de dejar el trabajo y seguir en la comunidad o de terminacion de trabajo por company o por dev
+
+                  Meter ya contratos reales
+                </p>
+
+              <h2>Monthly payments</h2>
+              <p>Despues vamos a cargar el equivalente a los que pusiste en finalSalary</p>
+              <ul>
+                  <li />
+                </ul>
+              <h1>Definition of professional reasons</h1>
+              <h2> these terms are the only ones acceptable for quitting once you accept </h2>
+
+              <ul>
+                  <li>eee</li>
+                  <li>eee</li>
+                </ul>
+
+              <h1>FAQ</h1>
+              <h3>Can the developer quit?</h3>
+              <p>While using Our Service, We may ask You to provide Us with certain personally identifiable information that can be used to contact or identify You. Personally identifiable information may include, but is not limited to:</p>
+              <h1>Contact Us</h1>
+              <p>If you have any questions about this Payment Terms, You can contact us:</p>
+              <ul>
+                  <li>By email: support@totaldevs.com</li>
+                </ul>
+
+            </div>
+
+          <Footer />
+        </>
+  )
+}
