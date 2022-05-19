0c96dd8752eb84fccb10252b9e7a9c89e089449a
carlo <85521045+lobotomisador@users.noreply.github.com>
2022-05-18 21:44:42 -0500

    90 totalresume v2 (#93)
    * adds button to hire dev
    
    * Commenting log with a watch
    
    Co-authored-by: Alejandro Sosa <asosacas@gmail.com>

--- pages/resumes/index.js
+++ pages/resumes/index.js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 import TotalResume from '@/components/resume'
 
-export default function Resumes() {
+export default function Resumes ({ handleCreateJobPosting }) {
   const router = useRouter()
-  return <TotalResume {...router.query} />
+  return <TotalResume {...router.query} handleCreateJobPosting={handleCreateJobPosting} />
 }
